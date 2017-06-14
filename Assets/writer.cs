656412364edb08661403a6c9f9bbd8567b01c249
arjun <arjuntalpallikar@utexas.edu>
2017-06-14 10:58:05 -0400

    first commit

---
+++ Assets/writer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class writer : MonoBehaviour {
+
+    Animator anim;
+    public Transform bits;
+	// Use this for initialization
+	void Start () {
+        anim = gameObject.GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        //Debug.Log(anim.GetBoneTransform());
+        //System.IO.File.WriteAllText("path", contents)	
+        Debug.Log(anim.GetBoneTransform());
+       
+	}
+}
