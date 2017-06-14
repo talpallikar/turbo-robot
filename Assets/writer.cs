59c46bdeef38b2b916f20996671fa26c619510bb
arjun <arjuntalpallikar@utexas.edu>
2017-06-14 15:26:43 -0400

    added traverse

--- Assets/writer.cs
+++ Assets/writer.cs
@@ -8,31 +8,38 @@ public class writer : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         anim = gameObject.GetComponent<Animator>();
+        Transform hipTransform = anim.GetBoneTransform(HumanBodyBones.Hips);
+
 
 
     }
 
     // Update is called once per frame
     void Update () {
+        
 
 
 
         //System.IO.File.WriteAllText("path", contents)
 
-        Transform hipTransform = anim.GetBoneTransform(HumanBodyBones.Hips);
+    
 
 
-        UnityEngine.Quaternion toPrint = anim.GetBoneTransform(HumanBodyBones.Hips).rotation;
 
-        Debug.Log(hipTransform.GetChildCount());
+        //UnityEngine.Quaternion toPrint = anim.GetBoneTransform(HumanBodyBones.Hips).rotation;
 
-        Debug.Log((Time.frameCount).ToString() + (toPrint));
+        //Debug.Log(hipTransform.GetChildCount());
+
+        //Debug.Log((Time.frameCount).ToString() + (toPrint));
 
     }
 
-    void traverse ()
+    void traverse (Transform parent)
     {
-
+        foreach (Transform child in parent)
+        {
+            Debug.Log(child.ToString());
+        }
     }
 
 
