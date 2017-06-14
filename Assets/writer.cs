805d53a8eaf08f773d6168d82a3f10eed581a871
arjun <arjuntalpallikar@utexas.edu>
2017-06-14 14:45:23 -0400

    updated scripting

--- Assets/writer.cs
+++ Assets/writer.cs
@@ -5,19 +5,35 @@ using UnityEngine;
 public class writer : MonoBehaviour {
 
     Animator anim;
-    public Transform bits;
 	// Use this for initialization
 	void Start () {
         anim = gameObject.GetComponent<Animator>();
+
 
     }
 
     // Update is called once per frame
     void Update () {
 
-        //Debug.Log(anim.GetBoneTransform());
-        //System.IO.File.WriteAllText("path", contents)	
-        Debug.Log(anim.GetBoneTransform());
-       
-	}
+
+
+        //System.IO.File.WriteAllText("path", contents)
+
+        Transform hipTransform = anim.GetBoneTransform(HumanBodyBones.Hips);
+
+
+        UnityEngine.Quaternion toPrint = anim.GetBoneTransform(HumanBodyBones.Hips).rotation;
+
+        Debug.Log(hipTransform.GetChildCount());
+
+        Debug.Log((Time.frameCount).ToString() + (toPrint));
+
+    }
+
+    void traverse ()
+    {
+
+    }
+
+
 }
