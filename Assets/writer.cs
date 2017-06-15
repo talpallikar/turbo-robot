517159b60a0af78c02dfd0d2a81a4a468f957851
arjun <arjuntalpallikar@utexas.edu>
2017-06-15 15:30:44 -0400

    working data pipeline

--- Assets/writer.cs
+++ Assets/writer.cs
@@ -1,46 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
-public class writer : MonoBehaviour {
 
-    Animator anim;
-	// Use this for initialization
-	void Start () {
-        anim = gameObject.GetComponent<Animator>();
-        Transform hipTransform = anim.GetBoneTransform(HumanBodyBones.Hips);
+public class writer : MonoBehaviour
+{
 
+    //Animator anim;
+    string path;
 
-
-    }
-
-    // Update is called once per frame
-    void Update () {
-        
-
-
-
-        //System.IO.File.WriteAllText("path", contents)
-
-    
-
-
-
+    // Use this for initialization
+    void Start()
+    {
+        //anim = gameObject.GetComponent<Animator>();
         //UnityEngine.Quaternion toPrint = anim.GetBoneTransform(HumanBodyBones.Hips).rotation;
-
-        //Debug.Log(hipTransform.GetChildCount());
-
         //Debug.Log((Time.frameCount).ToString() + (toPrint));
 
     }
 
-    void traverse (Transform parent)
+    // Update is called once per frame
+    void Update()
     {
-        foreach (Transform child in parent)
+        path = @"C:\Users\bobth\Documents\College\Summer17\vr\motion-capture\Assets\results.csv";
+        using (StreamWriter sw = File.AppendText(path))
         {
-            Debug.Log(child.ToString());
+            Transform[] allChildren = GetComponentsInChildren<Transform>();
+            foreach (Transform child in allChildren)
+            {
+                string rotation = child.rotation.ToString().Substring(1,child.rotation.ToString().Length-2);
+                string component = child.ToString().Substring(0, child.ToString().Length - 24);
+                sw.WriteLine(component+',' + rotation +','+ Time.frameCount.ToString());
+            }
         }
     }
-
-
 }
+
+
