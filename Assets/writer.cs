aafe84d69c257e89c6515371145f9cffc6a75291
arjun <arjuntalpallikar@utexas.edu>
2017-06-16 15:00:08 -0400

    added mecanim strategy and refactored gamecomponent approach

--- Assets/writer.cs
+++ Assets/writer.cs
@@ -7,33 +7,58 @@ using UnityEngine;
 public class writer : MonoBehaviour
 {
 
-    //Animator anim;
+    Animator anim;
     string path;
 
     // Use this for initialization
     void Start()
     {
-        //anim = gameObject.GetComponent<Animator>();
-        //UnityEngine.Quaternion toPrint = anim.GetBoneTransform(HumanBodyBones.Hips).rotation;
-        //Debug.Log((Time.frameCount).ToString() + (toPrint));
+        anim = gameObject.GetComponent<Animator>();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        path = @"C:\Users\bobth\Documents\College\Summer17\vr\motion-capture\Assets\results.csv";
+        path = @"C:\Users\bobth\Documents\College\Summer17\vr\motion-capture\Results\results.csv";
         using (StreamWriter sw = File.AppendText(path))
         {
-            Transform[] allChildren = GetComponentsInChildren<Transform>();
-            foreach (Transform child in allChildren)
+            //GCPrint(sw);
+            MecanimPrint(sw);
+        }
+    }
+
+    //recursively get all children of the GO, and then call FmtPrint()
+    void GCPrint(StreamWriter sw)
+    {
+        Transform[] allChildren = GetComponentsInChildren<Transform>();
+        foreach (Transform child in allChildren)
+        {
+            string rotation = child.rotation.ToString().Substring(1, child.rotation.ToString().Length - 2);
+            string component = child.ToString().Substring(0, child.ToString().Length - 24);
+            FmtPrint(sw, rotation, component);
+        }
+    }
+
+    //iterate over HumanBodyBones enumerable, and then call FmtPrint()
+    void MecanimPrint(StreamWriter sw)
+    {
+        foreach (HumanBodyBones bone in System.Enum.GetValues(typeof(HumanBodyBones)))
+        {
+            if (anim.GetBoneTransform(bone)!=null)
             {
-                string rotation = child.rotation.ToString().Substring(1,child.rotation.ToString().Length-2);
-                string component = child.ToString().Substring(0, child.ToString().Length - 24);
-                sw.WriteLine(component+',' + rotation +','+ Time.frameCount.ToString());
+                string rotation = anim.GetBoneTransform(bone).rotation.ToString();
+                string component = bone.ToString();
+                FmtPrint(sw, rotation, component);
             }
         }
     }
+
+    void FmtPrint(StreamWriter sw, string rotation, string component)
+    {
+        sw.WriteLine(component + ',' + rotation + ',' + Time.frameCount.ToString());
+    }
+
 }
 
 
